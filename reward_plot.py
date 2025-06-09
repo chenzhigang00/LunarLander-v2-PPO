a25753301b2be5b1a868993c1c39125729679541
chenzhigang00 <1312383034@qq.com>
2025-06-09 22:03:02 +0800

    plot

--- reward_plot.py
+++ reward_plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-rewards = np.load("LunarLander-v2-PPO/rewards.npy")
+rewards = np.load("LunarLander-v2-PPO/rewards_3.npy")
 plt.plot(rewards)
 plt.xlabel("Episode")
 plt.ylabel("Reward")
